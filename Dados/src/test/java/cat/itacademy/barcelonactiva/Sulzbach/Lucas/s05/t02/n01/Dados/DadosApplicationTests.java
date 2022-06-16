c5eb74195ba7b4c9a95e9b2a836d48acb6b6eda4
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-06-16 10:28:12 +0200

    S5.02.N01 - initial files

---
+++ Dados/src/test/java/cat/itacademy/barcelonactiva/Sulzbach/Lucas/s05/t02/n01/Dados/DadosApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Sulzbach.Lucas.s05.t02.n01.Dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

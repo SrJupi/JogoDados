016b9582d619afea7863dd570b136e4c08bbd63a
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-06-23 10:51:44 +0200

    clean entity

--- Dados/src/main/java/cat/itacademy/barcelonactiva/Sulzbach/Lucas/s05/t02/n01/Dados/model/domain/PlayerEntity.java
+++ Dados/src/main/java/cat/itacademy/barcelonactiva/Sulzbach/Lucas/s05/t02/n01/Dados/model/domain/PlayerEntity.java
@@ -22,12 +22,10 @@ public class PlayerEntity {
     private Integer userId;
     private String name;
     private Timestamp registerDate;
-    private Integer gamesWon;
-    private Integer gamesPlayed;
 
     @JsonManagedReference
     @OneToMany(mappedBy="player")
-    Set <GameEntity> gamesList = new HashSet<>();
+    private Set <GameEntity> gamesList = new HashSet<>();
 
     public PlayerEntity() {
     }
@@ -35,16 +33,6 @@ public class PlayerEntity {
     public PlayerEntity(String name) {
         this.name = name;
         this.registerDate = setRegisterDate();
-        this.gamesWon = 0;
-        this.gamesPlayed = 0;
-    }
-
-    public void addGamePlayed (){
-        this.gamesPlayed += 1;
-    }
-
-    public void addGameWon (){
-        this.gamesWon += 1;
     }
 
     private Timestamp setRegisterDate (){
